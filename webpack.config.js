ce0a33e4f2acf18707eadb88bb4eab1cf50c704a
act1gmr <act1gmr@googlemail.com>
2020-12-14 21:35:42 +0330

    - major project upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -91,7 +91,7 @@ module.exports = (e, argv) => {
         },
         {
           test: /\.(png|jpg|gif|ttf|eot|woff2|woff|mp3|svg)$/,
-          exclude: /(oneone|layers|layers-2x|marker-icon)\.png/,
+          exclude: /(oneone|layers|layers-2x|marker-icon|marker-icon-2x)\.png/,
           use: [
             {
               loader: "url-loader",
@@ -114,7 +114,7 @@ module.exports = (e, argv) => {
           ]
         },
         {
-          test: /(layers|layers-2x|marker-icon|marker-shadow)\.png/,
+          test: /(layers|layers-2x|marker-icon|marker-shadow|marker-icon-2x)\.png/,
           use: {
             loader: "file-loader",
             options: {
